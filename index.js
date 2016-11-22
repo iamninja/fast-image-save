933b2e7c46ab7722338095edee00788a91fcc646
Vagios Vlachos <bagios234@gmail.com>
2016-11-22 03:23:33 +0200

    Make button toggle. Add panel to button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var self = require("sdk/self");
-var buttons = require("sdk/ui/button/action");
+var actionButtons = require("sdk/ui/button/action");
+var toggleButtons = require("sdk/ui/button/toggle");
+var panels = require("sdk/panel");
 
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
@@ -9,17 +11,31 @@ function dummy(text, callback) {
 
 exports.dummy = dummy;
 
-var button = buttons.ActionButton({
+var button = toggleButtons.ToggleButton({
   id: "fast-save-pref-button",
   label: "Fast Image Save",
   icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
+    "16": self.data.url("icon/icon-16.png"),
+    "32": self.data.url("icon/icon-32.png"),
+    "64": self.data.url("icon/icon-64.png")
   },
   onClick: handleActionButtonClick
 });
 
+var settingsPanel = panels.Panel({
+  contentURL: self.data.url("html/settingsPanel.html"),
+  onHide: handleSettingsPanelHide
+});
+
 function handleActionButtonClick(state) {
   console.log("Button clicked...");
+  if (state.checked) {
+    settingsPanel.show({
+      position: button
+    });
+  }
+}
+
+function handleSettingsPanelHide() {
+  button.state('window', { checked: false });
 }
