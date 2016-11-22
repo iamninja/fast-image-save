3d1b06768c265e89e31142f3572ab8edcdd0b316
Vagios Vlachos <bagios234@gmail.com>
2016-11-22 02:34:08 +0200

    Add action button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var self = require("sdk/self");
+var buttons = require("sdk/ui/button/action");
 
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
@@ -7,3 +8,18 @@ function dummy(text, callback) {
 }
 
 exports.dummy = dummy;
+
+var button = buttons.ActionButton({
+  id: "fast-save-pref-button",
+  label: "Fast Image Save",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: handleActionButtonClick
+});
+
+function handleActionButtonClick(state) {
+  console.log("Button clicked...");
+}
